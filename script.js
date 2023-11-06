bc446d6063e612f1fefd9745a1a1021b76b79d1d
Qori Hidayat <hidayatqori@gmail.com>
2023-11-06 20:20:41 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const input = '120000';
+const data = 'Hallo Apakabar Semuanya';
+const number = Number(input);
+
+console.log(typeof number);
+console.log(number.toString(2));
+console.log(number.toLocaleString("id-ID"));
+console.log(data.toLowerCase());
+console.log(data.charAt(3));
+console.log(data.split(" "));
